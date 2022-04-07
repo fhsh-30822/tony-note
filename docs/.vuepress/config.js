cc83d6387392ad3506131e3101c2b210bf898c0a
unknown <fhsh-30822>
2022-04-07 08:39:04 +0800

    更改標題

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = { 
+    title: "tony's note", 
+    description: "Github 及 Vercel 上架筆記"}
+    module.exports = {
+        theme: 'vuepress-theme-foo',
+        theme: 'bar',
+        theme: path.resolve(__dirname, './path/to/local/theme'),
+      }
